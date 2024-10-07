4aa06cf90b927fa03c49c6011b76a40070c15a2e
MuhammetIncirkus <164883670+MuhammetIncirkus@users.noreply.github.com>
2024-10-07 11:16:33 +0200

    Ticket 1.0 done

---
+++ SmartHome/Views/SmartHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SmartHomeView.swift
+//  SmartHome
+//
+//  Created by Muhammet Incirkus on 07.10.24.
+//
+
+import SwiftUI
+
+struct SmartHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmartHomeView()
+}
